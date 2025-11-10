9fb4d871b9dc136ff6359bd99708b4e5eca79f52
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-11-10 15:57:48 -0600

    adjusted css on recipes page

--- src/components/recipes/Recipes.tsx
+++ src/components/recipes/Recipes.tsx
@@ -10,7 +10,7 @@ export const Recipes = () => {
     }, [])
     return (
         <div className="p-10">
-            <h1 className="text-center text-6xl font-bold tracking-wider md:text-7xl">
+            <h1 className="text-center text-6xl font-bold tracking-wider md:mt-15 md:text-6xl">
                 Recipes
             </h1>
             <div className="mt-10 flex flex-wrap items-start justify-around gap-5 md:mt-20 md:gap-10">
